bd3ccb75bbffecbb57c910d9d6fa35436a6c511b
Atarox <80927839+Atarox228@users.noreply.github.com>
2026-02-17 21:01:53 -0300

    modificacion para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/escobar-community-hub/" : "/",
+export default defineConfig({
+  base: "/",
 
   server: {
     host: "::",
@@ -14,10 +13,11 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
+
